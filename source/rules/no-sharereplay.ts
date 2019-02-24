c00810db2351d5aa4c23c5986d1c1bb62695dec9
Nicholas Jamieson <nicholas@cartant.com>
2019-02-24 11:58:33 +1000

    feat: Implement no-sharereplay.

--- source/rules/no-sharereplay.ts
+++ source/rules/no-sharereplay.ts
@@ -2,38 +2,55 @@
  * @license Use of this source code is governed by an MIT-style license that
  * can be found in the LICENSE file at https://github.com/cartant/eslint-plugin-rxjs
  */
+/* tslint:disable:object-literal-sort-keys */
 
 import { Rule } from "eslint";
+import * as ESTree from "estree";
 
 const rule: Rule.RuleModule = {
     meta: {
         docs: {
-            category: "Fill me in",
-            description: "Test",
-            recommended: false
+            category: "RxJS",
+            description: "Forbids using the `shareReplay` operator.",
+            recommended: true
         },
-        fixable: null, // or "code" or "whitespace"
-        schema: [
-            // fill in your schema
-        ]
+        fixable: null,
+        messages: {
+            forbidden: "shareReplay is forbidden.",
+            forbiddenWithoutConfig: "shareReplay is forbidden unless a config argument is passed."
+        },
+        schema: [{
+            properties: {
+                allowConfig: { type: "boolean" }
+            },
+            type: "object"
+        }]
     },
-
     create: context => {
-
-        // variables should be defined here
-
-        //----------------------------------------------------------------------
-        // Helpers
-        //----------------------------------------------------------------------
-
-        // any helper functions should go here or else delete this section
-
-        //----------------------------------------------------------------------
-        // Public
-        //----------------------------------------------------------------------
-
+        const [config] = context.options;
+        const { allowConfig = false } = config || {};
+        const sourceCode = context.getSourceCode();
+        function isShareReplayCall(node: ESTree.CallExpression): boolean {
+            const { callee } = node;
+            return (callee.type === "Identifier") && (sourceCode.getText(callee) === "shareReplay");
+        }
         return {
-            // give me methods
+            CallExpression: (node: ESTree.CallExpression) => {
+                if (isShareReplayCall(node)) {
+                    let report = true;
+                    if (allowConfig) {
+                        report = (node.arguments.length !== 1) || (node.arguments[0].type !== "ObjectExpression");
+                    }
+                    if (report) {
+                        context.report({
+                            messageId: allowConfig
+                                ? "forbiddenWithoutConfig"
+                                : "forbidden",
+                            node
+                        });
+                    }
+                }
+            }
         };
     }
 };
--- tests/rules/no-sharereplay.ts
+++ tests/rules/no-sharereplay.ts
@@ -6,18 +6,104 @@
 import { RuleTester } from "eslint";
 import rule = require("../../source/rules/no-sharereplay");
 
-const ruleTester = new RuleTester();
+const ruleTester = new RuleTester({
+    parserOptions: { ecmaVersion: 6 }
+});
 ruleTester.run("no-sharereplay", rule, {
-    valid: [
-        // give me some code that won't trigger a warning
-    ],
-    invalid: [
-        {
-            code: "/*no-sharereplay*/",
-            errors: [{
-                message: "Fill me in.",
-                type: "Me too"
-            }]
-        }
-    ]
+    valid: [{
+        code: `
+            const shared = of(42).pipe(
+                shareReplay({ refCount: true })
+            );
+        `,
+        options: [{ allowConfig: true }]
+    }, {
+        code: `
+            const shared = of(42).pipe(
+                shareReplay({ refCount: false })
+            );
+        `,
+        options: [{ allowConfig: true }]
+    }],
+    invalid: [{
+        code: `
+            const shared = of(42).pipe(
+                shareReplay()
+            );
+        `,
+        errors: [{
+            column: 17,
+            line: 3,
+            messageId: "forbidden"
+        }]
+    }, {
+        code: `
+            const shared = of(42).pipe(
+                shareReplay()
+            );
+        `,
+        errors: [{
+            column: 17,
+            line: 3,
+            messageId: "forbiddenWithoutConfig"
+        }],
+        options: [{ allowConfig: true }]
+    }, {
+        code: `
+            const shared = of(42).pipe(
+                shareReplay(1)
+            );
+        `,
+        errors: [{
+            column: 17,
+            line: 3,
+            messageId: "forbidden"
+        }]
+    }, , {
+        code: `
+            const shared = of(42).pipe(
+                shareReplay(1, 100)
+            );
+        `,
+        errors: [{
+            column: 17,
+            line: 3,
+            messageId: "forbidden"
+        }]
+    }, {
+        code: `
+            const shared = of(42).pipe(
+                shareReplay(1, 100, asapScheduler)
+            );
+        `,
+        errors: [{
+            column: 17,
+            line: 3,
+            messageId: "forbidden"
+        }]
+    }, {
+        code: `
+            const shared = of(42).pipe(
+                shareReplay({ refCount: true })
+            );
+        `,
+        errors: [{
+            column: 17,
+            line: 3,
+            messageId: "forbidden"
+        }],
+        options: [{ allowConfig: false }]
+    }, {
+        code: `
+            const shared = of(42).pipe(
+                shareReplay({ refCount: false })
+            );
+        `,
+        errors: [{
+            column: 17,
+            line: 3,
+            messageId: "forbidden"
+        }],
+        options: [{ allowConfig: false }]
+    }]
 });
