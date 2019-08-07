d0003347c8b69b06b4354981710885e1d2e9483a
timdeschryver <28659384+timdeschryver@users.noreply.github.com>
2019-08-07 13:24:50 +0200

    feat: add no-compat

---
+++ source/rules/no-compat.ts
@@ -0,0 +1,37 @@
+/**
+ * @license Use of this source code is governed by an MIT-style license that
+ * can be found in the LICENSE file at https://github.com/cartant/eslint-plugin-rxjs
+ */
+
+import { Rule } from "eslint";
+import * as es from "estree";
+
+const rule: Rule.RuleModule = {
+  meta: {
+    docs: {
+      category: "RxJS",
+      description:
+        "Disallows importation from locations that depend upon 'rxjs-compat'.",
+      recommended: true
+    },
+    fixable: null,
+    messages: {
+      forbidden: "'rxjs-compat' dependent import locations are forbidden."
+    },
+    schema: []
+  },
+  create: context => {
+    return {
+      "ImportDeclaration Literal[value=/^rxjs\\u002f/]:not(Literal[value=/^rxjs\\u002f(ajax|operators|testing|webSocket)/])": (
+        node: es.Literal
+      ) => {
+        context.report({
+          messageId: "forbidden",
+          node
+        });
+      }
+    };
+  }
+};
+
+export = rule;
