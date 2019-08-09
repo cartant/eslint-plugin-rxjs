20db3c6da5d8bf195ca45f094cc890d2bf7808ec
Tim Deschryver <28659384+timdeschryver@users.noreply.github.com>
2019-08-09 10:47:14 +0200

    feat: add no-ignored-subscribe  (#5)
    * feat: add no-ignored-subscription
    
    * refactor: introduce getTypeCheckerAndNodeMap
    
    * chore: feedback changes

---
+++ source/rules/no-ignored-subscribe.ts
@@ -0,0 +1,46 @@
+/**
+ * @license Use of this source code is governed by an MIT-style license that
+ * can be found in the LICENSE file at https://github.com/cartant/eslint-plugin-rxjs
+ */
+
+import { Rule } from "eslint";
+import * as es from "estree";
+import * as ts from "typescript";
+import { couldBeType } from "tsutils-etc";
+import { getTypeCheckerAndNodeMap } from "../utils";
+
+const rule: Rule.RuleModule = {
+  meta: {
+    docs: {
+      category: "RxJS",
+      description: "Disallows the calling of subscribe without specifying arguments.",
+      recommended: false
+    },
+    fixable: null,
+    messages: {
+      forbidden: "Calling subscribe without arguments is forbidden."
+    },
+    schema: []
+  },
+  create: context => {
+    const { nodeMap, typeChecker } = getTypeCheckerAndNodeMap(context);
+
+    return {
+      "ExpressionStatement[expression.callee.property.name='subscribe']:has([expression.arguments.length = 0]) > CallExpression > MemberExpression": (
+        node: es.MemberExpression
+      ) => {
+        const identifier = nodeMap.get(node.object) as ts.Identifier;
+        const identifierType = typeChecker.getTypeAtLocation(identifier);
+
+        if (couldBeType(identifierType, "Observable")) {
+          context.report({
+            messageId: "forbidden",
+            node: node.property
+          });
+        }
+      }
+    };
+  }
+};
+
+export = rule;
