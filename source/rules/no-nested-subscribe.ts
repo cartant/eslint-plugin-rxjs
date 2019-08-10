d83eef1733544ffdb3310144d286bda996d25895
timdeschryver <28659384+timdeschryver@users.noreply.github.com>
2019-08-10 21:09:14 +0200

    feat: add no-nested-subscribe

---
+++ source/rules/no-nested-subscribe.ts
@@ -0,0 +1,65 @@
+/**
+ * @license Use of this source code is governed by an MIT-style license that
+ * can be found in the LICENSE file at https://github.com/cartant/eslint-plugin-rxjs
+ */
+
+import { Rule } from "eslint";
+import * as es from "estree";
+import { couldBeType } from "tsutils-etc";
+import { getTypeCheckerAndNodeMap } from "../utils";
+const esquery = require("esquery");
+
+const rule: Rule.RuleModule = {
+  meta: {
+    docs: {
+      category: "RxJS",
+      description:
+        "Disallows the calling of `subscribe` within a `subscribe` callback.",
+      recommended: true
+    },
+    fixable: null,
+    messages: {
+      forbidden: "Nested subscribe calls are forbidden."
+    },
+    schema: []
+  },
+  create: context => {
+    const { nodeMap, typeChecker } = getTypeCheckerAndNodeMap(context);
+    const subscribeQuery =
+      "CallExpression > MemberExpression[property.name='subscribe']";
+
+    return {
+      [subscribeQuery]: (node: es.MemberExpression) => {
+        const identifier = nodeMap.get(node.object);
+        const identifierType = typeChecker.getTypeAtLocation(identifier);
+
+        if (!couldBeType(identifierType, "Observable")) {
+          return;
+        }
+
+        const callExpression = (node as any).parent as es.CallExpression;
+        callExpression.arguments.forEach(childNode => {
+          const childNodes: es.MemberExpression[] = esquery(
+            childNode,
+            subscribeQuery
+          );
+          childNodes.forEach(childNode => {
+            const childIdentifier = nodeMap.get(childNode.object);
+            const childIdentifierType = typeChecker.getTypeAtLocation(
+              childIdentifier
+            );
+
+            if (couldBeType(childIdentifierType, "Observable")) {
+              context.report({
+                messageId: "forbidden",
+                node: childNode.property
+              });
+            }
+          });
+        });
+      }
+    };
+  }
+};
+
+export = rule;
---
+++ tests/rules/no-nested-subscribe.ts
@@ -0,0 +1,202 @@
+/**
+ * @license Use of this source code is governed by an MIT-style license that
+ * can be found in the LICENSE file at https://github.com/cartant/eslint-plugin-rxjs
+ */
+
+import { stripIndent } from "common-tags";
+import rule = require("../../source/rules/no-nested-subscribe");
+import { ruleTester } from "../utils";
+
+ruleTester({ types: true }).run("no-nested-subscribe", rule, {
+  valid: [
+    `
+      import { Observable } from "rxjs";
+
+      of(47).subscribe(value => {
+        console.log(value);
+      })
+    `,
+    `
+      import { Observable } from "rxjs";
+
+      of(47).subscribe({
+        next: value => console.log(value),
+        error: value => console.log(value),
+        complete: value => console.log(value),
+      })
+    `,
+    `
+      import { Observable } from "rxjs";
+
+      const observableSubscribe = Observable.prototype.subscribe;
+      expect(Observable.prototype.subscribe).to.equal(observableSubscribe);
+    `
+  ],
+  invalid: [
+    {
+      code: stripIndent`
+        import { of } from "rxjs";
+
+        of("foo").subscribe(
+            value => of("bar").subscribe()
+        );
+      `,
+      errors: [
+        {
+          messageId: "forbidden",
+          line: 4,
+          column: 24,
+          endLine: 4,
+          endColumn: 33
+        }
+      ]
+    },
+    {
+      code: stripIndent`
+        import { of } from "rxjs";
+
+        of("foo").subscribe({
+            next: value => of("bar").subscribe()
+        });
+      `,
+      errors: [
+        {
+          messageId: "forbidden",
+          line: 4,
+          column: 30,
+          endLine: 4,
+          endColumn: 39
+        }
+      ]
+    },
+    {
+      code: stripIndent`
+        import { of } from "rxjs";
+
+        of("foo").subscribe({
+            next(value) { of("bar").subscribe(); }
+        });
+      `,
+      errors: [
+        {
+          messageId: "forbidden",
+          line: 4,
+          column: 29,
+          endLine: 4,
+          endColumn: 38
+        }
+      ]
+    },
+    {
+      code: stripIndent`
+        import { of } from "rxjs";
+
+        of("foo").subscribe(
+            undefined,
+            error => of("bar").subscribe()
+        );
+      `,
+      errors: [
+        {
+          messageId: "forbidden",
+          line: 5,
+          column: 24,
+          endLine: 5,
+          endColumn: 33
+        }
+      ]
+    },
+    {
+      code: stripIndent`
+        import { of } from "rxjs";
+
+        of("foo").subscribe({
+          error: error => of("bar").subscribe()
+        });
+      `,
+      errors: [
+        {
+          messageId: "forbidden",
+          line: 4,
+          column: 29,
+          endLine: 4,
+          endColumn: 38
+        }
+      ]
+    },
+    {
+      code: stripIndent`
+        import { of } from "rxjs";
+
+        of("foo").subscribe({
+          error(error) { of("bar").subscribe(); }
+        });
+      `,
+      errors: [
+        {
+          messageId: "forbidden",
+          line: 4,
+          column: 28,
+          endLine: 4,
+          endColumn: 37
+        }
+      ]
+    },
+    {
+      code: stripIndent`
+        import { of } from "rxjs";
+
+        of("foo").subscribe(
+          undefined,
+          undefined,
+          () => of("bar").subscribe()
+        );
+      `,
+      errors: [
+        {
+          messageId: "forbidden",
+          line: 6,
+          column: 19,
+          endLine: 6,
+          endColumn: 28
+        }
+      ]
+    },
+    {
+      code: stripIndent`
+        import { of } from "rxjs";
+
+        of("foo").subscribe({
+          complete: () => of("bar").subscribe()
+        });
+      `,
+      errors: [
+        {
+          messageId: "forbidden",
+          line: 4,
+          column: 29,
+          endLine: 4,
+          endColumn: 38
+        }
+      ]
+    },
+    {
+      code: stripIndent`
+        import { of } from "rxjs";
+
+        of("foo").subscribe({
+          complete() { of("bar").subscribe(); }
+        });
+      `,
+      errors: [
+        {
+          messageId: "forbidden",
+          line: 4,
+          column: 26,
+          endLine: 4,
+          endColumn: 35
+        }
+      ]
+    }
+  ]
+});
