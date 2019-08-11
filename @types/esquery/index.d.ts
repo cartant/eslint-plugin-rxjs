8dbe85231e8c17562f938efa1aec54009a04da5e
timdeschryver <28659384+timdeschryver@users.noreply.github.com>
2019-08-11 19:46:37 +0200

    refactor: improve esquery generics

--- @types/esquery/index.d.ts
+++ @types/esquery/index.d.ts
@@ -1,5 +1,8 @@
 declare module "esquery" {
   import * as es from "estree";
-  declare function esquery<T = es.Node>(node: es.Node, selector: string): T[];
+  declare function esquery<T extends es.Node = es.Node>(
+    node: es.Node,
+    selector: string
+  ): T[];
   export = esquery;
 }
