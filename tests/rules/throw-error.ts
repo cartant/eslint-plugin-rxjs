069c417ce41a27a9350a0b98c8c103e23114ba16
Tim Deschryver <28659384+timdeschryver@users.noreply.github.com>
2019-08-13 11:06:49 +0200

    feat: add throw-error (#13)
    * feat: add throw-error
    
    * feat: add Identifier case
    
    * chore: make rule not recommended

---
+++ tests/rules/throw-error.ts
@@ -0,0 +1,135 @@
+/**
+ * @license Use of this source code is governed by an MIT-style license that
+ * can be found in the LICENSE file at https://github.com/cartant/eslint-plugin-rxjs
+ */
+
+import { stripIndent } from "common-tags";
+import rule = require("../../source/rules/throw-error");
+import { ruleTester } from "../utils";
+
+ruleTester({ types: true }).run("throw-error", rule, {
+  valid: [
+    stripIndent`
+      const a = () => { throw new Error("error"); };
+    `,
+    stripIndent`
+      const a = () => { throw "error" as any };
+    `,
+    stripIndent`
+      const a = () => { throw errorMessage(); };
+
+      function errorMessage(): any {
+        return "error";
+      }
+    `,
+    stripIndent`
+      import { throwError } from "rxjs";
+
+      const ob1 = throwError(new Error("Boom!"));
+    `,
+    stripIndent`
+      import { throwError } from "rxjs";
+
+      const ob1 = throwError("Boom!" as any);
+    `,
+    stripIndent`
+      import { throwError } from "rxjs";
+
+      const ob1 = throwError(errorMessage());
+
+      function errorMessage(): any {
+        return "error";
+      }
+    `,
+    stripIndent`
+      // There will be no signature for callback and
+      // that should not effect and internal error.
+      declare const callback: Function;
+      callback();
+    `
+  ],
+  invalid: [
+    {
+      code: `const a = () => { throw "error"; };`,
+      errors: [
+        {
+          messageId: "forbidden",
+          line: 1,
+          column: 25,
+          endLine: 1,
+          endColumn: 32
+        }
+      ]
+    },
+    {
+      code: stripIndent`
+        const a = () => { throw errorMessage(); };
+
+        function errorMessage() {
+          return "error";
+        }
+      `,
+      errors: [
+        {
+          messageId: "forbidden",
+          line: 1,
+          column: 25,
+          endLine: 1,
+          endColumn: 39
+        }
+      ]
+    },
+    {
+      code: stripIndent`
+        const errorMessage = "Boom!";
+
+        const a = () => { throw errorMessage; };
+      `,
+      errors: [
+        {
+          messageId: "forbidden",
+          line: 3,
+          column: 25,
+          endLine: 3,
+          endColumn: 37
+        }
+      ]
+    },
+    {
+      code: stripIndent`
+        import { throwError } from "rxjs";
+
+        const ob1 = throwError("Boom!");
+      `,
+      errors: [
+        {
+          messageId: "forbidden",
+          line: 3,
+          column: 24,
+          endLine: 3,
+          endColumn: 31
+        }
+      ]
+    },
+    {
+      code: stripIndent`
+        import { throwError } from "rxjs";
+
+        const ob1 = throwError(errorMessage());
+
+        function errorMessage() {
+          return "Boom!";
+        }
+      `,
+      errors: [
+        {
+          messageId: "forbidden",
+          line: 3,
+          column: 24,
+          endLine: 3,
+          endColumn: 38
+        }
+      ]
+    }
+  ]
+});
