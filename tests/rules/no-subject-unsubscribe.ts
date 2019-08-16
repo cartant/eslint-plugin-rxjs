264f1f6240b9e531b4cc84da07ea170b8e0dc033
Tim Deschryver <28659384+timdeschryver@users.noreply.github.com>
2019-08-17 01:11:16 +0200

    feat: add no-subject-unsubscribe (#19)

---
+++ tests/rules/no-subject-unsubscribe.ts
@@ -0,0 +1,99 @@
+/**
+ * @license Use of this source code is governed by an MIT-style license that
+ * can be found in the LICENSE file at https://github.com/cartant/eslint-plugin-rxjs
+ */
+
+import { stripIndent } from "common-tags";
+import rule = require("../../source/rules/no-subject-unsubscribe");
+import { ruleTester } from "../utils";
+
+ruleTester({ types: true }).run("no-subject-unsubscribe", rule, {
+  valid: [
+    stripIndent`
+      import { Subject } from "rxjs";
+
+      const a = new Subject<number>();
+      const asub = a.subscribe();
+      asub.unsubscribe();
+    `,
+    stripIndent`
+      import { AsyncSubject } from "rxjs";
+
+      const a = new AsyncSubject<number>();
+      const asub = a.subscribe();
+      asub.unsubscribe();
+    `
+  ],
+  invalid: [
+    {
+      code: stripIndent`
+        import { Subject } from "rxjs";
+
+        const b = new Subject<number>();
+        b.unsubscribe();
+      `,
+      errors: [
+        {
+          messageId: "forbidden",
+          line: 4,
+          column: 3,
+          endLine: 4,
+          endColumn: 14
+        }
+      ]
+    },
+    {
+      code: stripIndent`
+        import { AsyncSubject } from "rxjs";
+
+        const b = new AsyncSubject<number>();
+        b.unsubscribe();
+      `,
+      errors: [
+        {
+          messageId: "forbidden",
+          line: 4,
+          column: 3,
+          endLine: 4,
+          endColumn: 14
+        }
+      ]
+    },
+    {
+      code: stripIndent`
+        import { Subject, Subscription } from "rxjs";
+
+        const csub = new Subscription();
+        const c = new Subject<number>();
+        csub.add(c);
+      `,
+      errors: [
+        {
+          messageId: "forbidden",
+          line: 5,
+          column: 10,
+          endLine: 5,
+          endColumn: 11
+        }
+      ]
+    },
+    {
+      code: stripIndent`
+        import { AsyncSubject, Subscription } from "rxjs";
+
+        const csub = new Subscription();
+        const c = new AsyncSubject<number>();
+        csub.add(c);
+      `,
+      errors: [
+        {
+          messageId: "forbidden",
+          line: 5,
+          column: 10,
+          endLine: 5,
+          endColumn: 11
+        }
+      ]
+    }
+  ]
+});
