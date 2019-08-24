7dbe497f3a38d41e7db41348a6616f04f1e270f4
timdeschryver <28659384+timdeschryver@users.noreply.github.com>
2019-08-24 21:50:37 +0200

    feat: make regexp case insensitive

--- source/rules/ban-observables.ts
+++ source/rules/ban-observables.ts
@@ -39,7 +39,7 @@ const rule: Rule.RuleModule = {
       if (value !== false) {
         bans.push({
           explanation: typeof value === "string" ? value : "",
-          regExp: new RegExp(`^${key}$`)
+          regExp: new RegExp(`^${key}$`, "i")
         });
       }
     });
--- tests/rules/ban-observables.ts
+++ tests/rules/ban-observables.ts
@@ -7,14 +7,15 @@ import rule = require("../../source/rules/ban-observables");
 import { ruleTester } from "../utils";
 
 ruleTester({ types: false }).run("ban-observables", rule, {
-  valid: [`import { concat, merge as m } from "rxjs";`],
+  valid: [`import { of, Observable } from "rxjs";`],
   invalid: [
     {
-      code: `import { concat, merge as m } from "rxjs";`,
+      code: `import { of, Observable as o, Subject } from "rxjs";`,
       options: [
         {
-          concat: true,
-          merge: "because I say so"
+          of: true,
+          observable: "because I say so",
+          subject: false
         }
       ],
       errors: [
@@ -23,20 +24,20 @@ ruleTester({ types: false }).run("ban-observables", rule, {
           line: 1,
           column: 10,
           endLine: 1,
-          endColumn: 16,
+          endColumn: 12,
           data: {
-            name: "concat",
+            name: "of",
             explanation: ""
           }
         },
         {
           messageId: "forbidden",
           line: 1,
-          column: 18,
+          column: 14,
           endLine: 1,
-          endColumn: 23,
+          endColumn: 24,
           data: {
-            name: "merge",
+            name: "Observable",
             explanation: ": because I say so"
           }
         }
