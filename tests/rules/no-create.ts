e4fe2fe4bae5922c33b513033b9c4f994b9fab25
timdeschryver <28659384+timdeschryver@users.noreply.github.com>
2019-08-25 19:59:34 +0200

    feat: add no-create

---
+++ tests/rules/no-create.ts
@@ -0,0 +1,33 @@
+/**
+ * @license Use of this source code is governed by an MIT-style license that
+ * can be found in the LICENSE file at https://github.com/cartant/eslint-plugin-rxjs
+ */
+
+import { stripIndent } from "common-tags";
+import rule = require("../../source/rules/no-create");
+import { ruleTester } from "../utils";
+
+ruleTester({ types: true }).run("no-create", rule, {
+  invalid: [
+    {
+      code: stripIndent`
+        import { Observable, Observer } from "rxjs";
+
+        const ob = Observable.create((observer: Observer<string>) => {
+            observer.next("Hello, world.");
+            observer.complete();
+            return () => {};
+        });
+      `,
+      errors: [
+        {
+          messageId: "forbidden",
+          line: 3,
+          column: 23,
+          endLine: 3,
+          endColumn: 29
+        }
+      ]
+    }
+  ]
+});
