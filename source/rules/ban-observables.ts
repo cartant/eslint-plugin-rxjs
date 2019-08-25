5b253bce4d2ed38253f9219beb81d2e0fd9c102b
Tim Deschryver <28659384+timdeschryver@users.noreply.github.com>
2019-08-25 13:32:50 +0200

    feat: add ban-observables (#26)
    * feat: add ban-observables
    
    * perf: don't run rule if there is no config
    
    * feat: make regexp case insensitive
    
    * revert: case insensitive

---
+++ source/rules/ban-observables.ts
@@ -0,0 +1,78 @@
+/**
+ * @license Use of this source code is governed by an MIT-style license that
+ * can be found in the LICENSE file at https://github.com/cartant/eslint-plugin-rxjs
+ */
+
+import { stripIndent } from "common-tags";
+import { Rule } from "eslint";
+import * as es from "estree";
+
+const rule: Rule.RuleModule = {
+  meta: {
+    docs: {
+      category: "RxJS",
+      description: "Disallows the use of banned observables.",
+      recommended: false
+    },
+    fixable: null,
+    messages: {
+      forbidden: "RxJS observable is banned: {{name}}{{explanation}}."
+    },
+    schema: [
+      {
+        type: "object",
+        description: stripIndent`
+          An object containing keys that are names of observable factory functions
+          and values that are either booleans or strings containing the explanation for the ban.`
+      }
+    ]
+  },
+  create: context => {
+    let bans: { explanation: string; regExp: RegExp }[] = [];
+
+    const [config] = context.options;
+    if (!config) {
+      return {};
+    }
+
+    Object.entries(config).forEach(([key, value]) => {
+      if (value !== false) {
+        bans.push({
+          explanation: typeof value === "string" ? value : "",
+          regExp: new RegExp(`^${key}$`)
+        });
+      }
+    });
+
+    function getFailure(name: string) {
+      for (let b = 0, length = bans.length; b < length; ++b) {
+        const ban = bans[b];
+        if (ban.regExp.test(name)) {
+          const explanation = ban.explanation ? `: ${ban.explanation}` : "";
+          return {
+            messageId: "forbidden",
+            data: { name, explanation }
+          };
+        }
+      }
+      return undefined;
+    }
+
+    return {
+      "ImportDeclaration[source.value='rxjs'] > ImportSpecifier": (
+        node: es.ImportSpecifier
+      ) => {
+        const identifier = node.imported;
+        const failure = getFailure(identifier.name);
+        if (failure) {
+          context.report({
+            ...failure,
+            node: identifier
+          });
+        }
+      }
+    };
+  }
+};
+
+export = rule;
---
+++ tests/rules/ban-observables.ts
@@ -0,0 +1,47 @@
+/**
+ * @license Use of this source code is governed by an MIT-style license that
+ * can be found in the LICENSE file at https://github.com/cartant/eslint-plugin-rxjs
+ */
+
+import rule = require("../../source/rules/ban-observables");
+import { ruleTester } from "../utils";
+
+ruleTester({ types: false }).run("ban-observables", rule, {
+  valid: [`import { of, Observable } from "rxjs";`],
+  invalid: [
+    {
+      code: `import { of, Observable as o, Subject } from "rxjs";`,
+      options: [
+        {
+          of: true,
+          Observable: "because I say so",
+          Subject: false
+        }
+      ],
+      errors: [
+        {
+          messageId: "forbidden",
+          line: 1,
+          column: 10,
+          endLine: 1,
+          endColumn: 12,
+          data: {
+            name: "of",
+            explanation: ""
+          }
+        },
+        {
+          messageId: "forbidden",
+          line: 1,
+          column: 14,
+          endLine: 1,
+          endColumn: 24,
+          data: {
+            name: "Observable",
+            explanation: ": because I say so"
+          }
+        }
+      ]
+    }
+  ]
+});
