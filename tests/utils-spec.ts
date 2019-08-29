b4e0bd943aa2e771609170a5574263d0419eac05
Nicholas Jamieson <nicholas@cartant.com>
2019-08-29 12:08:25 +1000

    chore: Update license comment.

--- tests/utils-spec.ts
+++ tests/utils-spec.ts
@@ -1,6 +1,6 @@
 /**
  * @license Use of this source code is governed by an MIT-style license that
- * can be found in the LICENSE file at https://github.com/cartant/rxjs-tslint-rules
+ * can be found in the LICENSE file at https://github.com/cartant/eslint-plugin-rxjs
  */
 
 import { expect } from "chai";
