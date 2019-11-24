8f1ea4c4e2dc48a053fd8893b5a8c95da6bb2993
Nicholas Jamieson <nicholas@cartant.com>
2019-11-24 10:49:16 +1000

    test: Use ruleTester from eslint-etc.

--- tests/utils.ts
+++ tests/utils.ts
@@ -1,30 +1,8 @@
 /**
  * @license Use of this source code is governed by an MIT-style license that
- * can be found in the LICENSE file at https://github.com/cartant/eslint-plugin-etc
+ * can be found in the LICENSE file at https://github.com/cartant/eslint-plugin-rxjs
  */
 
-import { RuleTester } from "eslint";
-import { join } from "path";
+import { createRuleTester } from "eslint-etc";
 
-export function ruleTester({ types }: { types: boolean }) {
-  const filename = join(__dirname, "file.ts");
-  const tester = new RuleTester({
-    parser: join(__dirname, "../node_modules/@typescript-eslint/parser"),
-    parserOptions: {
-      ecmaVersion: 6,
-      project: types ? join(__dirname, "./tsconfig.json") : undefined,
-      sourceType: "module"
-    }
-  });
-  const run = tester.run;
-  tester.run = (name, rule, { invalid = [], valid = [] }) =>
-    run.call(tester, name, rule, {
-      invalid: invalid.map(test => ({ ...test, filename })),
-      valid: valid.map(test =>
-        typeof test === "string"
-          ? { code: test, filename }
-          : { ...test, filename }
-      )
-    });
-  return tester;
-}
+export const ruleTester = createRuleTester({});
