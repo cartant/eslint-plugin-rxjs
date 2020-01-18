af612e97ab96b713e61ffba16ad42ae526402ba8
Nicholas Jamieson <nicholas@cartant.com>
2020-01-18 13:33:45 +1000

    test: Add failing test for type arguments

--- tests/rules/no-explicit-generics.ts
+++ tests/rules/no-explicit-generics.ts
@@ -27,6 +27,20 @@ ruleTester({ types: false }).run("no-explicit-generics", rule, {
         const f = new Notification("N", 42);
         const g = new Notification<number>("E", undefined, "Kaboom!");
         const h = new Notification<number>("C");`
+    },
+    {
+      code: stripIndent`
+        // with array and object literals
+        import { BehaviorSubject, Notification } from "rxjs";
+        const a = new BehaviorSubject<number[]>([42]);
+        const b = new BehaviorSubject<number[]>([]);
+        const c = new BehaviorSubject<{ answer: number }>({ answer: 42 });
+        const d = new BehaviorSubject<{ answer?: number }>({});
+        const e = new Notification<number[]>("N", [42]);
+        const f = new Notification<number[]>("N", []);
+        const g = new Notification<{ answer: number }>("N", { answer: 42 });
+        const h = new Notification<{ answer?: number }>("N", {});
+      `
     }
   ],
   invalid: [
