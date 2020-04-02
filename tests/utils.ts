94f79c4c2ef7511981fe759696483a6d321aee82
Nicholas Jamieson <nicholas@cartant.com>
2020-04-02 10:44:37 +1000

    chore: Use TSX file in rule tester.

--- tests/utils.ts
+++ tests/utils.ts
@@ -4,5 +4,8 @@
  */
 
 import { createRuleTester } from "eslint-etc";
+import { resolve } from "path";
 
-export const ruleTester = createRuleTester({});
+export const ruleTester = createRuleTester({
+  filename: resolve("./tests/file.tsx")
+});
