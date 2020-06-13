2e112ee8caebd3721dab8476dae5d45f8fa9ec98
Nicholas Jamieson <nicholas@cartant.com>
2020-06-14 09:47:57 +1000

    test: Add test for suffix-subjects issue.

--- tests/rules/suffix-subjects.ts
+++ tests/rules/suffix-subjects.ts
@@ -229,6 +229,35 @@ ruleTester({ types: true }).run("suffix-subjects", rule, {
         },
       ],
     },
+    {
+      code: stripIndent`
+        // https://github.com/cartant/rxjs-tslint-rules/issues/88
+        import { RouterStateSerializer } from '@ngrx/router-store';
+        import { Params, RouterStateSnapshot } from '@angular/router';
+
+        /**
+         * The RouterStateSerializer takes the current RouterStateSnapshot
+         * and returns any pertinent information needed. The snapshot contains
+         * all information about the state of the router at the given point in time.
+         * The entire snapshot is complex and not always needed. In this case, you only
+         * need the URL and query parameters from the snapshot in the store. Other items could be
+         * returned such as route parameters and static route data.
+         */
+        export interface RouterStateUrl {
+          url: string;
+          queryParams: Params;
+        }
+
+        export class CustomRouterStateSerializer implements RouterStateSerializer<RouterStateUrl> {
+          serialize(routerState: RouterStateSnapshot): RouterStateUrl {
+            const { url } = routerState;
+            const queryParams = routerState.root.queryParams;
+
+            return { url, queryParams };
+          }
+        }
+      `,
+    },
   ],
   invalid: [
     fromFixture(
