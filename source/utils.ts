a238c2d7cdc7de5fc63e6f95a741157924f20f2a
Rafael Santana <rafaelsantana3095@gmail.com>
2021-01-10 17:00:23 -0300

    docs: fix github link (#58)

--- source/utils.ts
+++ source/utils.ts
@@ -20,5 +20,5 @@ export function createRegExpForWords(config: string | string[]): RegExp | null {
 
 export const ruleCreator = ESLintUtils.RuleCreator(
   (name) =>
-    `https://github/cartant/eslint-plugin-rxjs/tree/main/docs/rules/${name}.md`
+    `https://github.com/cartant/eslint-plugin-rxjs/tree/main/docs/rules/${name}.md`
 );
